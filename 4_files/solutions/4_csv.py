88104e8308091b2934a0281945bee76844ebf6e4
Pawe Goliszewski <pawel.goliszewski@comarch.com>
2023-10-19 09:13:44 +0200

    feat: added rest of solution for block 4

---
+++ 4_files/solutions/4_csv.py
@@ -0,0 +1,32 @@
+# Wczytaj plik data.csv zawierający dane osób. Oblicz BMI dla każdej osoby
+# (masa dzielona przez wzrost_w_metrach_do_kwadratu), średnią tych wartości i wypisz je na standardowe wyjście.
+
+import numpy as np
+
+data: np.ndarray = np.genfromtxt("data.csv", dtype=None, delimiter=',', encoding='utf-8', skip_header=True)
+
+bmi_ndarray: np.ndarray = data[:, 3] / (data[:, 2] / 100) ** 2
+
+for person_id, bmi in zip(data[:, 0], bmi_ndarray):
+    print(f"Osoba o id {person_id} ma bmi {bmi:.2f}")
+
+# alternatywne rozwiązanie
+# for i in range(len(bmi_ndarray)):
+#     print(f"Osoba o id {data[i, 0]} ma bmi {bmi_ndarray[i]:.2f}")
+
+
+print(bmi_ndarray.mean())
+
+
+## alternatywne rozwiązanie
+
+import pandas as pd
+
+data: pd.DataFrame = pd.read_csv("data.csv", delimiter=",")
+data["bmi"] = data["weight(kg)"] / (data["height(cm)"] / 100) ** 2
+
+
+for person_id, bmi in zip(data["id"], data["bmi"]):
+    print(f"Osoba o id {person_id} ma bmi {bmi:.2f}")
+
+print(data["bmi"].mean())
---
+++ 4_files/solutions/5_read_all_files.py
@@ -0,0 +1,32 @@
+# Mamy funkcję read_all_files, która przyjmuje listę nazw plików i zwraca listę linii występujących we kolejno
+# wszytskich plikach.
+# Rozszerz ją następująco:
+# Funkcja przyjmuje dodatkowy opcjonalny argument typu bool ignore_errors. Gdy wartość tego argumentu jest True, to funkcja pomija
+# nieistniejące pliki.
+
+import os
+
+
+def read_all_files(filenames: list[str]) -> list[str]:
+    lines = []
+    for filename in filenames:
+        with open(filename, "r") as file:
+            lines.extend(file.readlines())
+    return lines
+
+
+def read_all_files_ignored(filenames: list[str]) -> list[str]:
+    lines = []
+    for filename in filenames:
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                lines.extend(file.readlines())
+    return lines
+
+
+assert read_all_files([]) == []
+assert read_all_files(["a.txt", "b.txt"]) == ['hakuna matata\n', 'no woman no cry\n', 'cucumber\n', 'python is awesome\n', 'no woman no cry\n', 'cucumber\n', 'python is awesome\n', 'hakuna\n', 'matata\n', 'no woman no cry\n', 'lets cry\n', 'cucumber']
+# print(read_all_files(["a.txt", "b.txt", "c.txt"])) # rzuci wyjątek
+assert read_all_files(["a.txt", "b.txt", "t.txt"], ignore_errors=True) == ['hakuna matata\n', 'no woman no cry\n', 'cucumber\n', 'python is awesome\n', 'no woman no cry\n', 'cucumber\n', 'python is awesome\n', 'hakuna\n', 'matata\n', 'no woman no cry\n', 'lets cry\n', 'cucumber']
+assert read_all_files(["t.txt"], ignore_errors=True) == []
+
