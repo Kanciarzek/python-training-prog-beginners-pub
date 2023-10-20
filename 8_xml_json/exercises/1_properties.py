11e0e4a7e6782c5dd955aa4def728e51d264a880
Pawe Goliszewski <pawel.goliszewski@comarch.com>
2023-10-20 09:30:08 +0200

    feat: added exercise for parsing xml

---
+++ 8_xml_json/exercises/1_properties.py
@@ -0,0 +1,8 @@
+# Wykorzystując XMLTree API Wczytaj host, port i hasło z pliku properties.xml z tagu database i wypisz je na standardowe
+# wyjście w postaci: "host:port -p hasło" np. "127.0.0.1:5000 -p encrypted_password".
+# Wersja rozbudowana:
+# Jeżeli jest ustwaiona któraś ze zmiennych środowiskowych APP_DATABASE_HOST, APP_DATABASE_PORT, APP_DATABASE_PASSWORD,
+# jest zdefiniowana, to wartość dla odpowiednio hosta, portu czy hasła jest pobierana ze zmiennej środowiskowej zamiast
+# tej z pliku xml.
+
+
