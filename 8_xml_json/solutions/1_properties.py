735bcd37d10e63eb287ebd7bc2178b6a95f250e6
Pawe Goliszewski <pawel.goliszewski@comarch.com>
2023-10-20 10:01:06 +0200

    feat: added solution for xml exercise

---
+++ 8_xml_json/solutions/1_properties.py
@@ -0,0 +1,23 @@
+# Wykorzystując XMLTree API Wczytaj host, port i hasło z pliku properties.xml z tagu database i wypisz je na standardowe
+# wyjście w postaci: "host:port -p hasło" np. "127.0.0.1:5000 -p encrypted_password".
+# Wersja rozbudowana:
+# Jeżeli jest ustwaiona któraś ze zmiennych środowiskowych APP_DATABASE_HOST, APP_DATABASE_PORT, APP_DATABASE_PASSWORD,
+# jest zdefiniowana, to wartość dla odpowiednio hosta, portu czy hasła jest pobierana ze zmiennej środowiskowej zamiast
+# tej z pliku xml.
+
+import xml.etree.ElementTree as ET
+import os
+
+document = ET.parse("properties.xml")
+
+databaseET = document.find("database")
+
+host = databaseET.find("host").text
+port = databaseET.find("port").text
+password = databaseET.find("password").text
+
+
+host = os.environ.get("APP_DATABASE_HOST", host)
+port = os.environ.get("APP_DATABASE_PORT", port)
+password = os.environ.get("APP_DATABASE_PASSWORD", password)
+print(f"{host}:{port} -p {password}")
